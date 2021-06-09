06c522d25127118002fa20f743a7a0afbfec493a
zeiod <76787324+zeiod@users.noreply.github.com>
2021-06-10 01:57:11 +0800

    add new alarm activity

---
+++ app/src/main/java/com/example/loginregistersos/Alarm_sound.java
@@ -0,0 +1,14 @@
+package com.example.loginregistersos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alarm_sound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_sound);
+    }
+}
