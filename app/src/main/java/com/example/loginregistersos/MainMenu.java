edc95eeab32f1a14385ffe74054803c2a713195e
Zelin <wongzelin.tiger@gmail.com>
2021-06-11 10:03:00 +0800

    sos button added

--- app/src/main/java/com/example/loginregistersos/MainMenu.java
+++ app/src/main/java/com/example/loginregistersos/MainMenu.java
@@ -1,13 +1,18 @@
 package com.example.loginregistersos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.Manifest;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.net.Uri;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -88,6 +93,16 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View v) {
                 finish();
                 System.exit(0);
+
+            }
+        });
+
+        sos_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "999";
+                Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                startActivity(phoneIntent);
 
             }
         });
